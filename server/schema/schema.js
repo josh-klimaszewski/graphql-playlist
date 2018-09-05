167afe55b5ad9a2c99971cb726fcbf0ac8327dbc
AzureAD\JoshuaKlimaszewski <joshua@autobooks.co>
2018-09-05 16:56:59 -0400

    type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,20 +6,24 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 //dummy data
 const books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "Name of the Wind 2", genre: "Fantasy", id: "2" },
-  { name: "Name of the Wind 3", genre: "Fantasy", id: "3" }
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
 ];
 
 const authors = [
-  { name: "patrick", age: 44, id: "1" },
-  { name: "brandon", age: 42, id: "2" },
-  { name: "harold", age: 66, id: "3" }
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" }
 ];
 
 const BookType = new GraphQLObjectType({
@@ -27,7 +31,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+        type: AuthorType,
+        resolve(parent, args) {
+            return _.find(authors, {id: parent.authorId})
+        }
+    }
   })
 });
 
@@ -36,7 +46,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+        type: new GraphQLList(BookType),
+        resolve(parent, args) {
+            return _.filter(books, {authorId: parent.id})
+        }
+    }
   })
 });
 
